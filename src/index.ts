1a2a68f40aaf0624e88c416c86af4595ff8a5738
Mikey <76266972+michei69@users.noreply.github.com>
2025-07-19 11:46:35 +0300

    [patch] revert SmuleEffects export
    due to it depending on yauzl, if imported inside of electron / webpack based stuff, it errors out. SmuleEffects isnt used either way, so no problem if removed from exports

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { Smule } from "./smule";
 import type { SmuleSession } from "./types/smule-types";
 import { SmuleDotCom } from "./smuledotcom";
 import { SmuleUrls } from "./smule-urls";
-import { SmuleEffects } from "./smule-effects";
+// import { SmuleEffects } from "./smule-effects";
 
-export { Smule, SmuleDotCom, SmuleUrls, SmuleEffects }
+export { Smule, SmuleDotCom, SmuleUrls }
 export type { SmuleSession }
 
 // TODO: move stuff that shouldn't be exported OUT of these files
