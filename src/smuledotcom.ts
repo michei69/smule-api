87e76ec4a09e805fb11c41bea102ed50fd6ee353
Mikey <76266972+michei69@users.noreply.github.com>
2025-07-19 12:01:24 +0300

    [patch] fix 418 I'm a teapot! for smuledotcom

--- src/smuledotcom.ts
+++ src/smuledotcom.ts
@@ -93,7 +93,8 @@ export class SmuleDotCom {
         this.axiosInst = axios.create({
             withCredentials: true,
             headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/237.84.2.178 Safari/537.36"
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/237.84.2.178 Safari/537.36",
+                "Accept": "" // this bitch returns 418 im a teapot! if you have accept set to anything for some fucking reason
             }
         })
         this.cookies = []
